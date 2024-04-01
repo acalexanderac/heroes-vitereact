7f6c8a1f73056295f5c36e44d2cc8a4981dc1758
Christian Arias <110419649+acalexanderac@users.noreply.github.com>
2024-04-01 17:41:30 -0600

    Added Tables

--- src/pages/heroes/agregar/page123.tsx
+++ src/pages/heroes/agregar/page123.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import Alignment from "../../../components/subtables/alignment";
+import Gender from "../../../components/subtables/gender";
+import Publishers from "../../../components/subtables/publisher";
 
 const HeroForm = () => {
   const navigate = useNavigate();
@@ -96,183 +99,202 @@ const HeroForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      <div>
-        <label
-          htmlFor="name"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Name
-        </label>
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="eye_color"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Eye Color
-        </label>
-        <input
-          type="text"
-          name="eye_color"
-          value={formData.eye_color}
-          onChange={handleChange}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="hair_color"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Hair Color
-        </label>
-        <input
-          type="text"
-          name="hair_color"
-          value={formData.hair_color}
-          onChange={handleChange}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
+    <div className="grid grid-cols-3 gap-4">
+      <div className="row-span-3 h-auto">
+        {" "}
+        {/* Adjusted height */}
+        <form onSubmit={handleSubmit} className=" pl-10">
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="eye_color"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Eye Color
+            </label>
+            <input
+              type="text"
+              name="eye_color"
+              value={formData.eye_color}
+              onChange={handleChange}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="hair_color"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Hair Color
+            </label>
+            <input
+              type="text"
+              name="hair_color"
+              value={formData.hair_color}
+              onChange={handleChange}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
 
-      <div>
-        <label
-          htmlFor="skin_color"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Skin Color
-        </label>
-        <input
-          type="text"
-          name="skin_color"
-          value={formData.skin_color}
-          onChange={handleChange}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="height"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Height
-        </label>
-        <input
-          type="number"
-          name="height"
-          value={formData.height}
-          onChange={handleChange}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="weight"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Weight
-        </label>
-        <input
-          type="number"
-          name="weight"
-          value={formData.weight}
-          onChange={handleChange}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="hair_color"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Race
-        </label>
-        <input
-          type="text"
-          name="race"
-          value={formData.race}
-          onChange={handleChange}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
+          <div>
+            <label
+              htmlFor="skin_color"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Skin Color
+            </label>
+            <input
+              type="text"
+              name="skin_color"
+              value={formData.skin_color}
+              onChange={handleChange}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="height"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Height
+            </label>
+            <input
+              type="number"
+              name="height"
+              value={formData.height}
+              onChange={handleChange}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="weight"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Weight
+            </label>
+            <input
+              type="number"
+              name="weight"
+              value={formData.weight}
+              onChange={handleChange}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="hair_color"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Race
+            </label>
+            <input
+              type="text"
+              name="race"
+              value={formData.race}
+              onChange={handleChange}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
 
-      <div>
-        <label
-          htmlFor="publisher_id"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Publisher ID
-        </label>
-        <input
-          type="number"
-          name="publisher_id"
-          value={formData.publisher_id}
-          onChange={handleChange}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="gender_id"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Gender ID
-        </label>
-        <input
-          type="number"
-          name="gender_id"
-          value={formData.gender_id}
-          onChange={handleChange}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
+          <div>
+            <label
+              htmlFor="publisher_id"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Publisher ID
+            </label>
+            <input
+              type="number"
+              name="publisher_id"
+              value={formData.publisher_id}
+              onChange={handleChange}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="gender_id"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Gender ID
+            </label>
+            <input
+              type="number"
+              name="gender_id"
+              value={formData.gender_id}
+              onChange={handleChange}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
 
-      <div>
-        <label
-          htmlFor="alignment_id"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Alignment ID
-        </label>
-        <input
-          type="number"
-          name="alignment_id"
-          value={formData.alignment_id}
-          onChange={handleChange}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
+          <div>
+            <label
+              htmlFor="alignment_id"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Alignment ID
+            </label>
+            <input
+              type="number"
+              name="alignment_id"
+              value={formData.alignment_id}
+              onChange={handleChange}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="hero_id"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Hero ID
+            </label>
+            <input
+              type="number"
+              name="hero_id"
+              value={formData.hero_id}
+              onChange={handleChange}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          {/* Repeat for other fields */}
+          <button
+            type="submit"
+            className="py-2 px-4 bg-red-500 text-white rounded hover:bg-red-700"
+          >
+            Submit
+          </button>
+        </form>
       </div>
-      <div>
-        <label
-          htmlFor="hero_id"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Hero ID
-        </label>
-        <input
-          type="number"
-          name="hero_id"
-          value={formData.hero_id}
-          onChange={handleChange}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
+      <div className="row-span-1 h-auto">
+        {" "}
+        {/* Adjusted height */}
+        <Gender />
       </div>
-      {/* Repeat for other fields */}
-      <button
-        type="submit"
-        className="py-2 px-4 bg-red-500 text-white rounded hover:bg-red-700"
-      >
-        Submit
-      </button>
-    </form>
+      <div className="row-span-1 h-auto">
+        {" "}
+        {/* Adjusted height */}
+        <Alignment />
+      </div>
+      <div className="col-span-2 col-start-2 row-start-2 flex justify-center items-center">
+        <Publishers />
+      </div>
+    </div>
   );
 };
 
